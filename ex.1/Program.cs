a6cf04975dd2f6219e23baafeba2970e339b3e56
lubov0avduhova <loveavduhova@gmail.com>
2022-01-26 22:23:55 +0700

    1 упражнение сделано

--- ex.1/Program.cs
+++ ex.1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Написать программу «Анкета». Последовательно задаются вопросы (имя, фамилия, возраст, рост, вес). 
+// В результате вся информация выводится в одну строчку:
+
+// запрашиваем информацию анкеты
+Console.Write ("Скажи, как тебя зовут? ");
+string name = Console.ReadLine();
+Console.Write ("Твоя фамилия? ");
+string lastname = Console.ReadLine();
+Console.Write ("Твой возраст? ");
+string age = Console.ReadLine();
+Console.Write ("Рост? ");
+string height = Console.ReadLine();
+Console.Write ("Вес? ");
+string weight = Console.ReadLine();
+
+//а) используем склеивание;
+Console.WriteLine ("А теперь результаты склеивания!");
+Console.WriteLine ("Твое имя: " + name + " Фамилия " + lastname +
+ " Возраст: " + age + " Рост: " + height + " Вес: " + weight);
+
+// б) используем форматированный вывод;
+Console.WriteLine ("А теперь результаты форматированного вывода!");
+Console.WriteLine ("Твое имя:{0}" + " Фамилия: {1}" + " Возраст: {2}" + " Рост: {3}" + " Вес: {4}",
+                name, lastname, age, height, weight);
+// в) используя вывод со знаком $
+Console.WriteLine ("А теперь результаты вывод со знаком $");
+Console.Write ($"Твое имя:{name} Фамилия: {lastname} Возраст: {age} Рост: {height} Вес: {weight}");
